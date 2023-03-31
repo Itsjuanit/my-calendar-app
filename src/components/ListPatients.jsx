de09ef9205dfc9213134a1da1062b19be40a7884
Juan Ignacio Tejada <jtejada@lubee.com.ar>
2023-03-31 09:34:31 -0300

    Se cambio favicon, algunos titulos.

--- src/components/ListPatients.jsx
+++ src/components/ListPatients.jsx
@@ -29,7 +29,7 @@ export const ListPatients = ({ patients, setPatient, deletePatient }) => {
           <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
           <p className="text-xl mt-5 mb-10 text-center">
             Comienza agregando tus{" "}
-            <span className="text-indigo-600 font-bold">pacientes y citas</span>
+            <span className="text-indigo-600 font-bold">pacientes.</span>
           </p>
         </>
       )}
