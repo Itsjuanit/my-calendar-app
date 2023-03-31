4b47fcb443925372aba9bbf0feaf34641ff4fcca
Juan Ignacio Tejada <jtejada@lubee.com.ar>
2023-03-31 09:32:53 -0300

    Se cambio favicon, algunos titulos.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,7 @@ export const Header = () => {
   return (
     <>
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-        Seguimiento pacientes{" "}
-        <span className="text-indigo-600">Veterinarias</span>
+        Seguimiento para tus <span className="text-indigo-600">MASCOTAS</span>
       </h1>
     </>
   );
--- src/components/ListPatients.jsx
+++ src/components/ListPatients.jsx
@@ -6,10 +6,12 @@ export const ListPatients = ({ patients, setPatient, deletePatient }) => {
     <div className="md:w-1/2 lg:w-3/5 h-[calc(100vh-4rem)] overflow-x-hidden overflow-y-auto pr-2">
       {patients && patients.length ? (
         <>
-          <h2 className="font-black text-3xl text-center">ListPatients</h2>
+          <h2 className="font-black text-3xl text-center">
+            Listado de Pacientes
+          </h2>
           <p className="text-xl mt-5 mb-10 text-center">
             Administra tus{" "}
-            <span className="text-indigo-600 font-bold">pacientes y citas</span>
+            <span className="text-indigo-600 font-bold">pacientes.</span>
           </p>
           {patients.map((patient) => {
             return (
